81464e52533b57a995acdca6aeac7cd640d5bd6a
nm <neil.mccoy@outlook.com>
2015-09-06 21:11:04 +1000

    Basic working test - End of tutorial part one.

---
+++ SeleniumTutorial/MyFirstTest.feature
@@ -0,0 +1,6 @@
+﻿Feature: MyFirstTest
+
+Scenario: SeeMuppets
+	Given I can navigate to Google
+	When I search for muppets
+	Then I should see an image
---
+++ SeleniumTutorial/MyFirstTestSteps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using FluentAssertions;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using TechTalk.SpecFlow;
+
+namespace SeleniumTutorial
+{
+    [Binding]
+    public class MyFirstTestSteps
+    {
+        private ChromeDriver _driver;
+
+        [Given(@"I can navigate to Google")]
+        public void GivenICanNavigateToGoogle()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl("http://google.com");
+        }
+
+        [When(@"I search for muppets")]
+        public void WhenISearchForMuppets()
+        {
+            _driver.FindElementById("lst-ib").SendKeys("muppets");
+            _driver.FindElementByClassName("lsb").Click();
+        }
+
+        [Then(@"I should see an image")]
+        public void ThenIShouldSeeAnImage()
+        {
+            _driver.FindElementsByTagName("img").Count.Should().BeGreaterThan(0);
+        }
+
+    }
+}
